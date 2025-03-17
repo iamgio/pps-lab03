77a3742c847bf6a91430b9ea82f9103256483575
Giorgio Garofalo <giogar2705@gmail.com>
2025-03-17 10:36:17 +0100

    Add zip

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -46,7 +46,10 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10], [] => []
      * E.g., [], [] => []
      */
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons((h1, h2), zip(t1, t2))
+      case (Nil(), _) => Nil()
+      case (_, Nil()) => Nil()
 
     /*
      * Concatenate two sequences
