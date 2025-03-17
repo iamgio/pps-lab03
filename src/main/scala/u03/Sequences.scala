06fcb78d1acabcb0300c0c9459a7dbc0431484d0
Giorgio Garofalo <giogar2705@gmail.com>
2025-03-17 10:23:58 +0100

    Implement skip

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -3,6 +3,8 @@ package u03
 import u03.Optionals.Optional
 import u03.Optionals.Optional.*
 
+import scala.annotation.tailrec
+
 object Sequences: // Essentially, generic linkedlists
 
   enum Sequence[E]:
@@ -33,7 +35,11 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10, 20, 30], 0 => [10, 20, 30]
      * E.g., [], 2 => []
      */
-    def skip[A](s: Sequence[A])(n: Int): Sequence[A] = ???
+    @tailrec
+    def skip[A](s: Sequence[A])(n: Int): Sequence[A] = s match
+      case _ if n == 0 => s
+      case Cons(_, t) => skip(t)(n - 1)
+      case _ => Nil()
 
     /*
      * Zip two sequences
