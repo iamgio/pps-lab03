b72fea58931ecac3c362ee28ec50372f4bd56e6d
Giorgio Garofalo <giogar2705@gmail.com>
2025-03-17 10:38:51 +0100

    Refactor zip

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -48,8 +48,7 @@ object Sequences: // Essentially, generic linkedlists
      */
     def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
       case (Cons(h1, t1), Cons(h2, t2)) => Cons((h1, h2), zip(t1, t2))
-      case (Nil(), _) => Nil()
-      case (_, Nil()) => Nil()
+      case _ => Nil()
 
     /*
      * Concatenate two sequences
